ceb92c96f04a752252f94e9b6171313e1b57f9b7
sandra-vera1 <sandra-vera1@hotmail.com>
2024-07-24 11:08:19 -0600

    Coded studio listings page (view-studios.html, view-studios.css,view-studios.js).

---
+++ js/view-studios.js
@@ -0,0 +1,38 @@
+let studios = [
+    {
+        name: "Bow Valley Recording Studio",
+        address: "345 - 6 Avenue SE, Calgary, AB T2G 4V1",
+        areaSquareMeters: 200,
+        type: "recording studio",
+        capacity: 10,
+        hasParking: "Yes",
+        reachableByPublicTransportation: "Yes",
+        availableForRent: "Yes",
+        rentalTerm: "hourly",
+        pricePerTerm: 50
+    },
+    {
+        name: "Artistic Dance Studio",
+        address: "123 Main Street, Vancouver, BC V6B 2T2",
+        areaSquareMeters: 150,
+        type: "dance studio",
+        capacity: 20,
+        hasParking: "No",
+        reachableByPublicTransportation: "Yes",
+        availableForRent: "No",
+        rentalTerm: "daily",
+        pricePerTerm: 200
+    },
+    {
+        name: "Theater Rehearsal Space",
+        address: "789 Elm Avenue, Toronto, ON M5J 2T9",
+        areaSquareMeters: 300,
+        type: "rehearsal space",
+        capacity: 15,
+        hasParking: "Yes",
+        reachableByPublicTransportation: "Yes",
+        availableForRent: "Yes",
+        rentalTerm: "weekly",
+        pricePerTerm: 800
+    }
+];
