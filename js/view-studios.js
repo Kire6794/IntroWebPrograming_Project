750f0909f3a3d5cc12db5b14c0ac9f5100523ba5
sandra-vera1 <sandra-vera1@hotmail.com>
2024-07-31 17:09:31 -0600

    1-Back-end code for display studios (view-studios.html) Version 1

--- js/view-studios.js
+++ js/view-studios.js
@@ -1,38 +1,61 @@
-let studios = [
-    {
-        name: "Bow Valley Recording Studio",
-        address: "345 - 6 Avenue SE, Calgary, AB T2G 4V1",
-        areaSquareMeters: 200,
-        type: "recording studio",
-        capacity: 10,
-        hasParking: "Yes",
-        reachableByPublicTransportation: "Yes",
-        availableForRent: "Yes",
-        rentalTerm: "hourly",
-        pricePerTerm: 50
-    },
-    {
-        name: "Artistic Dance Studio",
-        address: "123 Main Street, Vancouver, BC V6B 2T2",
-        areaSquareMeters: 150,
-        type: "dance studio",
-        capacity: 20,
-        hasParking: "No",
-        reachableByPublicTransportation: "Yes",
-        availableForRent: "No",
-        rentalTerm: "daily",
-        pricePerTerm: 200
-    },
-    {
-        name: "Theater Rehearsal Space",
-        address: "789 Elm Avenue, Toronto, ON M5J 2T9",
-        areaSquareMeters: 300,
-        type: "rehearsal space",
-        capacity: 15,
-        hasParking: "Yes",
-        reachableByPublicTransportation: "Yes",
-        availableForRent: "Yes",
-        rentalTerm: "weekly",
-        pricePerTerm: 800
+let aStudios = [];
+
+async function getStudios() {
+    try {
+        const response = await fetch('../json/studios.json');
+
+        if (!response.ok) {
+            throw new Error(response.statusText);
+        }
+        aStudios = await response.json();
+        fillDataStudios();
+    } catch (error) {
+        console.error('Error:', error);
     }
-];
+}
+
+const rowStudio = `<tr>
+        <td>{Name}</td>
+        <td>{Address}</td>
+        <td>{Area}</td>
+        <td>{Type}</td>
+        <td>{Capacity}</td>
+        <td>{Parking}</td>
+        <td>{PublicTransport}</td>
+        <td>{Available}</td>
+        <td>{RentalTerm}</td>
+        <td>{PricePerTerm}</td>
+        <td>{Actions}</td>
+    </tr>`;
+
+function fillDataStudios(){
+    let dataBodyTable = '';
+    aStudios.forEach(function(studio){
+        dataBodyTable = dataBodyTable.concat(replaceObjectRow(studio));
+    })
+    $("#studios tbody").html(dataBodyTable)
+}
+
+function replaceObjectRow(studio){
+    let row = rowStudio;
+    Object.entries(studio).forEach(function(property){ //Object.entries - property key-value pairs from object - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+        row = row.replace(`{${property[0]}}`, property[1]);//0 = property name; 1 = property value; string Interpolation - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+    });
+    row = row.replace('{Actions}', replaceActionRow(studio));
+    return row;
+}
+
+const iTagTable = '<i class="material-icons">{FontCode}</i>';
+const showInfoIconCode = 'visibility';
+const priceIconCode = 'paid';
+
+function replaceActionRow(studio){ //Google Fonts icon - https://fonts.google.com/icons?icon.set=Material+Icons&selected=Material+Icons+Outlined:home:&icon.size=24&icon.color=%23e8eaed
+    let actionRow = '';
+    actionRow = actionRow.concat(iTagTable.replace('{FontCode}', showInfoIconCode));
+    if(true){ //Use the user property to show action
+        actionRow = actionRow.concat(iTagTable.replace('{FontCode}', priceIconCode));
+    }
+    return actionRow;
+}
+
+getStudios();
